ab1e7abaef5e46310073d7c11e32604314ec65a1
Evelyn Bordeau <ebordeau@gmail.com>
2025-01-04 17:10:22 -0500

    default to root user

--- app/.well-known/atproto-did/route.ts
+++ app/.well-known/atproto-did/route.ts
@@ -1,40 +1,30 @@
 import { NextRequest } from 'next/server';
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../../../amplify/data/resource';
-import { Amplify } from 'aws-amplify';
-import outputs from '../../../amplify_outputs.json';
+// import { Amplify } from 'aws-amplify';
+// import outputs from '../../../amplify_outputs.json';
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export async function GET(request: NextRequest) {
     const hostname = request.headers.get('host')!;
     const segments = hostname.split('.');
-    const username = segments.length === 3 ? segments[0] : '';
+    const username = segments.length === 3 ? segments[0] : 'root';
 
-    console.log(`Hostname: ${hostname}`);
-    console.log(`Username: ${username}`);
-
-    let did;
-    if (username) {
-        const { data: user, errors } = await client.models.User.get({
-            username,
-        });
-        did = user?.did;
-    } else {
-        // TODO: Create account for @lgbtq.town and add its did here
-        // did = 'did:plc:iqa6bfednevr3dztjkz5nd2m';
-    }
-
-    if (did) {
-        return new Response(did, {
+    const { data: user, errors } = await client.models.User.get({
+        username,
+    });
+    if (user?.did) {
+        return new Response(user.did, {
             status: 200, 
             headers: {
                 'Content-Type': 'text/plain',
             }
         });
     }
+
     return new Response('Bad Request', {
         status: 400,
     });
